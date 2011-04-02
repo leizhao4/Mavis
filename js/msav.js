dd15df3b97a4c289fe55ebed431f910e0c614f5c
Lei Zhao <leizhao4@gmail.com>
2011-04-02 04:13:07 +0000

    refactoring

--- js/msav.js
+++ js/msav.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-  createAlignmentLayout("Multiple Sequence Alignment Visualization")
-  createAlignmentTable("api.pl?query=alignment");
-  createAlignmentColors("api.pl?query=colors");
+  drawAlignmentLayout("Multiple Sequence Alignment Visualization")
+  drawAlignmentTable("api.pl?query=alignment");
+  drawAlignmentColors("api.pl?query=colors");
 });
 
-function createAlignmentLayout(title) {
+function drawAlignmentLayout(title) {
   $("#alignment_container").html("<div id='alignment_header'>" + title + "</div>");
   $("#alignment_container").append("<table id='alignment_table' cellspacing='0'></table>");
   $("#alignment_container").append("<div id='alignment_footer'>" + new Date().toLocaleString() + "</div>");
 }
 
-function createAlignmentTable(apiUrl) {
+function drawAlignmentTable(apiUrl) {
   $.getJSON(apiUrl, function(alignment) {
+    $("#alignment_table").empty();
     $.each(alignment, function(i, sequence) {
-      var rowHtml = "<tr><td id='seq_hue_" + sequence.row + "'>&nbsp;</td>" +
-                    "<td class='seq_name'>" + sequence.name + "</td>";
+      var rowHtml = "<tr><td id='seq_hue_" + sequence.row + "'>&nbsp;</td><td class='seq_name'>" + sequence.name + "</td>";
       $.each(sequence.seq.split(''), function(j, character) {
         var column = j + 1;
         rowHtml += "<td id='cell_" + sequence.row + "_" + column + "'>" + character + "</td>";
@@ -25,7 +25,7 @@ function createAlignmentTable(apiUrl) {
   });
 }
 
-function createAlignmentColors(apiUrl) {
+function drawAlignmentColors(apiUrl) {
   $.getJSON(apiUrl, function(colors) {
     $.each(colors, function(column, columnColors) {
       if (columnColors != null) {
