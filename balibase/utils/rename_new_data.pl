58be6621b8be87a15ca0869d4eebdc3ab999b29b
Lei Zhao <leizhao4@gmail.com>
2011-04-03 04:02:52 +0000

    remove old files

--- balibase/utils/rename_new_data.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-
-@files = `ls ../../data`;
-
-for my $file (@files) {
-  next unless $file =~ /([0-9]+)/;
-  my $number = $1;
-  system("svn rename ../../data/$number ../../data/balibase_$number");
-}
