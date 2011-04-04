61b4a0fdab049d261092a5ceae7fa18100b4b8fd
Lei Zhao <leizhao4@gmail.com>
2011-04-04 21:51:24 +0000

    some test data set, need to be removed after a better algorithm is done

---
+++ data/test/create_fake_scores.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+print "#COL_NUMBER\t#ROW_NUMBER_1\t#ROW_NUMBER_2\t#RES_PAIR_SCORE\n";
+
+for my $col (1 .. 21) {
+  for my $row1 (1 .. 10) {
+    for my $row2 (1 .. 10) {
+      next unless $row1 < $row2;
+      my $score = (21 - $col) * .05;
+      print "$col\t$row1\t$row2\t$score\n";
+    }
+  }
+}
