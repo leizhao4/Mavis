14d024a2964d36d9429896c941de80d523826ada
Lei Zhao <leizhao4@gmail.com>
2011-07-01 22:07:32 +0000

    changed sequence color back to be based on hue

--- MSAV/API.pm
+++ MSAV/API.pm
@@ -69,7 +69,7 @@ sub load_sequence_order {
   for my $seq_info (<ORDER>) {
     chomp $seq_info;
     my ($row, $seq_hue, $r, $g, $b, $l, $a_val, $b_val) = split /\t/, $seq_info;
-    push @$seq_order, { row => $row, color => &rgb_to_html($r, $g, $b), a => $a_val, b => $b_val };
+    push @$seq_order, { row => $row, color => &hue_to_html($seq_hue), a => $a_val, b => $b_val };
   }
   close ORDER;
   return $seq_order;
@@ -95,6 +95,22 @@ sub rgb_to_html {
   $r = 0 if $r < 0; $g = 0 if $g < 0; $b = 0 if $b < 0; 
   $r = 1 if $r > 1; $g = 1 if $g > 1; $b = 1 if $b > 1; 
   return sprintf "#%01x%01x%01x", int($r * 15.99), int($g * 15.99), int($b * 15.99);
+}
+
+sub hue_to_html {
+  my $hue = shift;
+  my $r   = &hue2prim($hue + 120);
+  my $g   = &hue2prim($hue);
+  my $b   = &hue2prim($hue - 120);
+  return &rgb_to_html($r, $g, $b);
+
+  sub hue2prim {
+    my $h = (shift) % 360;
+    if ($h < 60)  { return $h / 60; }
+    if ($h < 180) { return 1; }
+    if ($h < 240) { return (240 - $h) / 60; }
+    return 0;
+  }
 }
 
 1;
